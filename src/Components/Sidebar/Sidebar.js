4a7de14f83473f386661c47d7123745e9d1f31b0
WelcometoNoida <rajeshkumar05071999@gmail.com>
2023-03-04 16:35:37 +0530

    modified in sidenavbar

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -16,7 +16,56 @@ const Sidebar = () => {
          <NavIcon>
               <i className="fa fa-fw fa-home" style={{fontSize:"1.5em"}}></i>
          </NavIcon>
-         <NavText>Home</NavText>
+         <NavText>Dashboard</NavText>
+        </NavItem>
+
+        <NavItem>
+         <NavIcon>
+              <i className="fa fa-fw fa-message" style={{fontSize:"1.5em"}}></i>
+         </NavIcon>
+         <NavText>message</NavText>
+        </NavItem>
+
+        <NavItem>
+         <NavIcon>
+              <i className="fa fa-fw fa-user" style={{fontSize:"1.5em"}}></i>
+         </NavIcon>
+         <NavText>User Management</NavText>
+        </NavItem>
+
+        <NavItem>
+         <NavIcon>
+              <i className="fa fa-fw fa-bolt" style={{fontSize:"1.5em"}}></i>
+         </NavIcon>
+         <NavText>Credits</NavText>
+        </NavItem>
+
+        <NavItem>
+         <NavIcon>
+              <i className="fa fa-fw fa-bell" style={{fontSize:"1.5em"}}></i>
+         </NavIcon>
+         <NavText>Notification</NavText>
+        </NavItem>
+
+        <NavItem>
+         <NavIcon>
+              <i className="fa fa-fw fa-book" style={{fontSize:"1.5em"}}></i>
+         </NavIcon>
+         <NavText>Phonebook</NavText>
+        </NavItem>
+
+        <NavItem>
+         <NavIcon>
+              <i className="fa fa-fw fa-gear" style={{fontSize:"1.5em"}}></i>
+         </NavIcon>
+         <NavText>DLT</NavText>
+        </NavItem>
+
+        <NavItem>
+         <NavIcon>
+              <i className="fa fa-fw fa-tape" style={{fontSize:"1.5em"}}></i>
+         </NavIcon>
+         <NavText>API</NavText>
         </NavItem>
       </SideNav.Nav>
      </SideNav>
